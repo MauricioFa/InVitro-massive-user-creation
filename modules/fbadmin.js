859edb0fc5ab0820366c0e8d09fbd3d9810fdbb0
MauricioFa <mauriciositel@hotmail.com>
2020-05-29 22:14:14 -0500

    Firestore additional data user included

--- modules/fbadmin.js
+++ modules/fbadmin.js
@@ -1,51 +1,69 @@
-const admin = require("firebase-admin")
-require('dotenv').config()
+const admin = require("firebase-admin");
+require("dotenv").config();
 
-class multiUserCreation{
-  userCreation (jsonUsers){
+class multiUserCreation {
+  userCreation(jsonUsers) {
     if (!admin.apps.length) {
-        admin.initializeApp({
-          credential: admin.credential.cert({
-            "type": "service_account",
-            "project_id": process.env.PROJECT_ID,
-            "private_key_id": process.env.PRIVATE_KEY_ID,
-            "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-            "client_email": process.env.CLIENT_EMAIL,
-            "client_id": process.env.CLIENT_ID,
-            "auth_uri": process.env.AUTH_URI,
-            "token_uri": process.env.TOKEN_URI,
-            "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_CERT,
-            "client_x509_cert_url": process.env.CLIENT_CERT
-          }),
-          databaseURL: `https://${process.env.PROJECT_I}.firebaseio.com/`,
-        });
+      admin.initializeApp({
+        credential: admin.credential.cert({
+          type: "service_account",
+          project_id: process.env.PROJECT_ID,
+          private_key_id: process.env.PRIVATE_KEY_ID,
+          private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+          client_email: process.env.CLIENT_EMAIL,
+          client_id: process.env.CLIENT_ID,
+          auth_uri: process.env.AUTH_URI,
+          token_uri: process.env.TOKEN_URI,
+          auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT,
+          client_x509_cert_url: process.env.CLIENT_CERT,
+        }),
+        databaseURL: `https://${process.env.PROJECT_ID}.firebaseio.com/`,
+      });
     }
-    
-    async function insertUsers (jsonUsers){
-      let messages = []
-      for (let i=0; i < jsonUsers.length;i++){
+    const db = admin.firestore();
+
+    async function insertUsers(jsonUsers) {
+      let messages = [];
+      const users = jsonUsers
+      for (let i = 0; i < jsonUsers.length; i++) {
         const message = await admin
           .auth()
           .createUser({
-            email: jsonUsers[i]["Email"],
+            email: jsonUsers[i]["email"],
             emailVerified: false,
-            phoneNumber: `+57${jsonUsers[i]["Phone"]}`,
-            password: "secretPassword",
-            displayName: `${jsonUsers[i]["First Name"]} ${jsonUsers[i]["Last Name"]}`,
+            password: "ENtrANYmaTen",
             disabled: false,
           })
           .then(function (userRecord) {
             // See the UserRecord reference doc for the contents of userRecord.
-            // console.log("Successfully created new user:", userRecord.displayName);
-            return ({"User email":jsonUsers[i]["Email"], "Result":"Succesfully created"})
+            console.log("Successfully created new user:", userRecord.email, users);
+            // Create users data en firestore
+            db.collection("user").doc(users[i]["identityCard"]).set({
+              name: jsonUsers[i]["name"],
+              lastName: jsonUsers[i]["lastName"],
+              documentType: jsonUsers[i]["documentType"],
+              numberContact: jsonUsers[i]["numberContact"],
+              rol: jsonUsers[i]["rol"],
+              userStatus: true
+            })
+            .then(()=>{
+              console.log("succesfully inserted")
+            })
+            .catch((error)=>{
+              console.log(error)
+            })
+            return {
+              "User email": jsonUsers[i]["email"],
+              Result: "Succesfully created",
+            };
           })
           .catch(function (error) {
             // console.log("Error creating new user:", error);
-          return ({"User email":jsonUsers[i]["Email"], "Result":error.code})
-          })
-          messages.push(message)
-        }
-        return messages
+            return { "User email": jsonUsers[i]["email"], Result: error.code };
+          });
+        messages.push(message);
+      }
+      return messages;
     }
 
     // (async () => {
@@ -53,12 +71,12 @@ class multiUserCreation{
     //   console.log(messaFinal)
     //   return messaFinal
     // })()
-    const messageFinal = insertUsers (jsonUsers)
-    messageFinal.then(function(result){
+    const messageFinal = insertUsers(jsonUsers);
+    messageFinal.then(function (result) {
       // console.log(result)
-      return messageFinal
-    })
+      return messageFinal;
+    });
   }
 }
 
-module.exports = multiUserCreation
+module.exports = multiUserCreation;
